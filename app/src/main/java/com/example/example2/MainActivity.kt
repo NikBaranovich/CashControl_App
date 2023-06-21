a2a5483b4e0a98a1c7bed76457a77b35824f8704
NikBaranovich <archosgp1975@gmail.com>
2023-06-21 19:52:06 +0300

    Add toggle menu button

--- app/src/main/java/com/example/example2/MainActivity.kt
+++ app/src/main/java/com/example/example2/MainActivity.kt
@@ -5,8 +5,11 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.example2.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
@@ -22,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         val isFirstRun = sharedPrefs.getBoolean(KEY_FIRST_RUN, true)
 
         if (isFirstRun) {
-            // Показать форму или выполнить необходимые действия для первого запуска
             val welcomeIntent = Intent(this@MainActivity, Welcome::class.java)
             startActivity(welcomeIntent)
         }
@@ -30,5 +32,26 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
         val navView: NavigationView = findViewById(R.id.nav_view)
 
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.nav_expenses -> Toast.makeText(applicationContext, "Expenses!", Toast.LENGTH_LONG).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+        if(toggle.onOptionsItemSelected(item))
+            return true
+    }
+    fun menuButtonClick(view:View?) {
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout.openDrawer(GravityCompat.START)
     }
 }
